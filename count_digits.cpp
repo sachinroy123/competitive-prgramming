a39d7fcca2eddecdbf8a9c744d7860743b6732dc
Sachin Shakya <sachinshakya205001@gmail.com>
2021-10-17 09:13:10 +0530

    modified count_digit.cpp

--- count_digits.cpp
+++ count_digits.cpp
@@ -1,2 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+
+int main()
+{
+
+    long long num = 343422342342;
+
+//    this is log10 method to find the number of digits 
+    cout<<"by log method : "<<floor(log10(num)+1)<<endl;
+
+    // String method to find the number of digit in a number 
+    string str = to_string(num);
+    cout<<"by string method : "<<str.length()<<endl;
+
+    // Using loop
+    int count = 0;
+    while(num>0)
+    {
+        count++;
+        num /= 10; 
+    }
+    cout<<"*Using loop : "<<count<<endl;
+
+}
